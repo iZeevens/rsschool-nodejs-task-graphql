5ef93c5f0b47042e94e525ef22f6e87dfe5d63c7
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-31 01:46:09 +0500

    feat: add test rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import schema from './schema.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -17,6 +18,13 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
     async handler(req, reply) {
       const { query, variables } = req.body;
+
+      const document = parse(query);
+      const errors = validate(schema, document, [depthLimit(5)]);
+
+      if (errors.length > 0) {
+        return reply.status(400).send({ errors });
+      }
 
       const result = await graphql({
         schema,
